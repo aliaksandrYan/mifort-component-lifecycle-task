154798a7dbf2081b84f31ecf512f9fac57e0d4d9
Aliaksandr <aliaksandr.yan@yandex.by>
2018-11-25 20:50:23 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NameInputComponent } from './components/name-input/name-input.component';
 import { SkillItemComponent } from './components/skill-item/skill-item.component';
-import { TotalSumComponent } from './components/total-sum/total-sum.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
@@ -12,7 +11,6 @@ import {FormsModule} from '@angular/forms';
     AppComponent,
     NameInputComponent,
     SkillItemComponent,
-    TotalSumComponent
   ],
   imports: [
     BrowserModule,
